7af59887e49b987eeab57b2b424c045e4215c276
lcs-amorim <lcs.amorim.lima@gmail.com>
2018-03-16 13:17:10 -0300

    cadastro_user

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django import forms
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from core.models import Usuario
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ["codigo_u","nome_u","usuario","senha","email_u","cpf","telefone_u","endereco_u","news"]
+    search_fields = ["nome_u","usuario","cpf","email_u"]
+    filter_horizontal = []
+    ordering = ["codigo_u"]
+    list_filter = []
+
+admin.site.register(Usuario,UsuarioAdmin)
