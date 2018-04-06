79562c56ca2c052e637875cde10d048de154a405
GabsGeorge <gabriel.g.s.santos@hotmail.com>
2018-04-06 01:51:25 -0300

    Up

--- EasyParty/urls.py
+++ EasyParty/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import index 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls)
+    url(r'^admin/', admin.site.urls),
+    url(r'^index', index, name="index"),
+    url(r'^$', index)
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from core.models import Usuario
+
+def index(request):
+	return render(request, "index.html")		
+
+def Produto(request):
+	contexto = {
+		"users":Usuario.objects.all()
+	}
+	return render(request,"index.html", contexto)
 
 # Create your views here.
